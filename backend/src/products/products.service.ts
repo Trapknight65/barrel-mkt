ebf77b6c3b338da7b3e0ab743e7e20bb1a9f4e51
Allan Deschamps <allan.deschamps@outlook.fr>
2025-12-21 16:22:04 +0000

    Fix csv-parser import for Vercel build

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from '../entities/product.entity';
 import { Readable } from 'stream';
-import * as csv from 'csv-parser';
+import csv from 'csv-parser';
 
 @Injectable()
 export class ProductsService {
